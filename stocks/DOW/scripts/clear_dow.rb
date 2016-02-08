d5f3b04b8a8028fc163a14a371d2c0c98a80993a
Philip Vierhus <pvierhus@gmail.com>
2016-02-08 01:35:58 -0800

    stocks/DOW/scripts/clear_dow.rb now only clears out the 1 minute and 2 minute data so we will still have data on the day but not a huge ammount where it will croud the computer

--- stocks/DOW/scripts/clear_dow.rb
+++ stocks/DOW/scripts/clear_dow.rb
@@ -1,7 +1,9 @@
 root = "/home/phil/funsidestuff/stocks"
+day = `date | awk '{print $2"_"$3"_"$6}'`.chomp
 
 dow = `ls #{root}/DOW/stocks`.split(/\n/)
 
 dow.each { |stock|
-  `rm -rf #{root}/DOW/stocks/#{stock}/*`
+  `rm #{root}/DOW/stocks/#{stock}/#{day}/ery1min.data`
+  `rm #{root}/DOW/stocks/#{stock}/#{day}/ery2min.data`
 }
