cb0ee082ad20f488d45b895b56c28b2f37b47e53
Philip Vierhus <pvierhus@gmail.com>
2017-06-13 16:01:38 -0700

    fresh_install_setup.sh is now automatically says yes when using apt-get install.

--- fresh_install_setup.sh
+++ fresh_install_setup.sh
@@ -6,23 +6,27 @@ sudo apt-get update
 echo "******************************"
 echo "          GETTING VIM         "
 echo "******************************"
-sudo apt-get install vim
+sudo apt-get -y install vim
 
 echo "******************************"
 echo "          GETTING GIT         "
 echo "******************************"
-sudo apt-get install git
+sudo apt-get -y install git
 
 echo "******************************"
 echo "          GETTING RAILS       "
 echo "******************************"
-sudo apt-get install curl
+sudo apt-get -y install curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --rails
+\curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
+gem install --no-rdoc --no-ri rails
+sudo apt-get -y install node.js
 
 
 echo "PS1=\"\\[\\033[1;30m\\][\\T]\\[\\033[1;37m\\] \\[\\033[1;32m\\]\\u\\[\\033[1;33m\\]:\\[\\033[1;31m\\]\\w\\[\\033[1;37m\\] \\[\\033[1;32m\\]$\\[\\033[1;37m\\] \"" > ~/.bashrc
 
+echo >> ~/.bashrc
+echo "source \$HOME/.rvm/scripts/rvm" >> ~/.bashrc
 echo >> ~/.bashrc
 echo "alias ls='ls --color=always'" >> ~/.bashrc
 echo "alias l='ls'" >> ~/.bashrc
@@ -37,4 +41,29 @@ echo "alias mc='make clean'" >> ~/.bashrc
 echo >> ~/.bashrc
 echo "cd" >> ~/.bashrc
 
-rm -rf Desktop/ Documents/ Public/ Templates/ Videos/
+echo
+echo
+echo
+echo "**************LAST THING**************"
+echo "Removing Desktop, Documents, Public, Templates, and Videos directories in:"
+echo "10 (CTRL-C to stop)"
+sleep 1
+echo "9 (CTRL-C to stop)"
+sleep 1
+echo "8 (CTRL-C to stop)"
+sleep 1
+echo "7 (CTRL-C to stop)"
+sleep 1
+echo "6 (CTRL-C to stop)"
+sleep 1
+echo "5 (CTRL-C to stop)"
+sleep 1
+echo "4 (CTRL-C to stop)"
+sleep 1
+echo "3 (CTRL-C to stop)"
+sleep 1
+echo "2 (CTRL-C to stop)"
+sleep 1
+echo "1 (CTRL-C to stop)"
+sleep 1
+rm -rf Desktop/ Documents/ Public/ Templates/ Videos/ examples.desktop
