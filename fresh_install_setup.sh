cce00f1cf8eca2a244d099739869680f19c90697
Philip Vierhus <pvierhus@gmail.com>
2017-06-13 12:23:36 -0700

    Made the fresh_install_setup.sh a little cleaner.

--- fresh_install_setup.sh
+++ fresh_install_setup.sh
@@ -24,22 +24,17 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 echo "PS1=\"\\[\\033[1;30m\\][\\T]\\[\\033[1;37m\\] \\[\\033[1;32m\\]\\u\\[\\033[1;33m\\]:\\[\\033[1;31m\\]\\w\\[\\033[1;37m\\] \\[\\033[1;32m\\]$\\[\\033[1;37m\\] \"" > ~/.bashrc
 
 echo >> ~/.bashrc
-
 echo "alias ls='ls --color=always'" >> ~/.bashrc
 echo "alias l='ls'" >> ~/.bashrc
 echo "alias ll='ls -l'" >> ~/.bashrc
 echo "alias la='ls -a'" >> ~/.bashrc
 echo "alias lr='ls -R'" >> ~/.bashrc
-
 echo >> ~/.bashrc
-
 echo "alias c='clear'" >> ~/.bashrc
 echo "alias e='exit'" >> ~/.bashrc
 echo "alias m='make'" >> ~/.bashrc
 echo "alias mc='make clean'" >> ~/.bashrc
-
 echo >> ~/.bashrc
-
 echo "cd" >> ~/.bashrc
 
 rm -rf Desktop/ Documents/ Public/ Templates/ Videos/
