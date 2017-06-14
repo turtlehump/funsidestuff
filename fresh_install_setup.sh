299ff29cf3283c6d5b5d116b01033f705f1a222c
Philip Vierhus <pvierhus@gmail.com>
2017-06-13 17:26:39 -0700

    fresh_install_setup.sh installs rails and makes sure the directories that are deleted are in the home folder.

--- fresh_install_setup.sh
+++ fresh_install_setup.sh
@@ -18,10 +18,7 @@ echo "          GETTING RAILS       "
 echo "******************************"
 sudo apt-get -y install curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles
-gem install --no-rdoc --no-ri rails
-sudo apt-get -y install node.js
-
+\curl -sSL https://get.rvm.io | bash -s stable --rails --auto-dotfiles
 
 echo "PS1=\"\\[\\033[1;30m\\][\\T]\\[\\033[1;37m\\] \\[\\033[1;32m\\]\\u\\[\\033[1;33m\\]:\\[\\033[1;31m\\]\\w\\[\\033[1;37m\\] \\[\\033[1;32m\\]$\\[\\033[1;37m\\] \"" > ~/.bashrc
 
@@ -66,4 +63,4 @@ echo "2 (CTRL-C to stop)"
 sleep 1
 echo "1 (CTRL-C to stop)"
 sleep 1
-rm -rf Desktop/ Documents/ Public/ Templates/ Videos/ examples.desktop
+rm -rf ~/Desktop/ ~/Documents/ ~/Public/ ~/Templates/ ~/Videos/ ~/examples.desktop
