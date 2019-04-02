9d256c848e8cbf165a19dcd320cf2e89457952ea
Philip Vierhus <pvierhus@gmail.com>
2019-04-02 01:23:53 -0700

    Changed a few variable names on the loan calculator and added a summary of the results at the end.

--- money/loans/loan_calc.cpp
+++ money/loans/loan_calc.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
   cin >> money_borrowed;
   while(1)
   {
-    cout << "Whats the intrest rate? ";
+    cout << "Whats the intrest rate? (express as decimal)  ";
     cin >> intrest_rate;
     if(intrest_rate < 1 && intrest_rate > 0)
       break;
@@ -41,30 +41,34 @@ int main(int argc, char* argv[])
   {
     cout << "What do you want your payment per month to be? ";
     cin >> payment;
-    int i = 0;
-    double total = 0;
+    int months = 0;
+    double total_paid = 0;
     double money_left = money_borrowed;
     while(money_left > payment)
     {
       money_left -= payment;
       money_left += (money_left * monthly_intrest_rate);
-      total += payment;
-      i++;
-      //cout << "After " << i << " payments (months) of " << payment << ", you still owe " << setprecision(2) << fixed << money_left << endl;
+      total_paid += payment;
+      months++;
+      cout << "After " << months << " payments (months) of " << payment << ", you still owe " << setprecision(2) << fixed << money_left << endl;
     }
-    total += money_left;
+    total_paid += money_left;
     cout << endl;
-    cout << "This loan of " << money_borrowed << " will take " << i << " months to pay off when making equal payments of " << payment << endl;
+    cout << "This loan of " << money_borrowed << " will take " << months << " months to pay off when making equal payments of " << payment << endl;
     cout << "Your last payment will be " << setprecision(2) << fixed << money_left << endl;
-    cout << "You end up paying " << setprecision(2) << fixed << total << endl;
-    double money_made = total - money_borrowed;
-    double percent_made = 100 * (money_made / money_borrowed);
-    cout << "The bank makes " << money_made << ", or %" << percent_made << endl;
+    cout << "You end up paying " << setprecision(2) << fixed << total_paid << endl;
+
+    double bank_money_made = total_paid - money_borrowed;
+    double bank_percent_made = 100 * (bank_money_made / money_borrowed);
+    cout << "The bank makes " << bank_money_made << ", or %" << bank_percent_made << ", over " << months << " months." << endl;
+    double bank_percent_made_per_month = bank_percent_made / months;
+    cout << "(Thats %" << bank_percent_made_per_month << " per month.)" << endl;
   }
   else //tmp == "-p"
   {
     cout << "How many months do you want it to take to pay off your loan of " << money_borrowed << "? ";
     cin >> months;
+    cout << endl << endl << "Sorry, this is not implemented yet." << endl;
   }
   return 0;
 }
