2819b476f07e6ef40ebaa52c80e5dfe9c1d2b93e
Philip Vierhus <pvierhus@gmail.com>
2022-01-16 15:28:29 +1100

    Broke up encrypter main.cpp into smaller/easier functions (proper C++)

--- encryption/main.cpp
+++ encryption/main.cpp
@@ -7,31 +7,23 @@ using namespace std;
 
 void intro_description();
 
-//I wouldnt have to do all this if people would just input properly...
 int get_encryption_option();
 
-int run_single_key_option();
-int run_RSA_option();
+int run_single_key();
+int run_single_key_encrypt();
+int run_single_key_decrypt();
+
+int run_RSA();
 
 int main()
 {
   intro_description();
 
-  int option = get_encryption_option();
+  int option = get_encryption_option(); //can only return 1 or 2
 
-  if(option == 1)
-  {
-    return run_single_key_option();
-  }
-  else if(option == 2)
-  {
-    return run_RSA_option();
-  }
-  else
-  {
-    cout << "Error: Got out of get_encryption_option() with invalid option." << endl;
-    return 1;
-  }
+  if      (option == 1) return run_single_key();
+  else if (option == 2) return run_RSA();
+  else                  return 1; //There was an error in selecting the encryption type
 }
 
 void intro_description()
@@ -44,7 +36,7 @@ void intro_description()
   cout << "further communication within that first message. You should use single-key" << endl;
   cout << "encryption for all further communication with that same person." << endl << endl;
 
-  cout << "Note: The single key should be longer than 15 characters so it is difficult" << endl;
+  cout << "NOTE: The single key should be longer than 15 characters so it is difficult" << endl;
   cout << "to crack." << endl << endl;
 
   return;
@@ -62,6 +54,7 @@ int get_encryption_option()
 
     getline(cin, option);
     option.erase(remove(option.begin(), option.end(), ' '), option.end());
+    option.erase(remove(option.begin(), option.end(), '\t'), option.end());
     cout << endl;
 
     if      (option == "1") return 1;
@@ -70,100 +63,90 @@ int get_encryption_option()
   }while(1);
 }
 
-int run_single_key_option()
+int run_single_key()
 {
-  Encrypter* encrypter = new Encrypter();
   cout << "************************* SINGLE-KEY *************************" << endl << endl;
 
-  int option;
+  string option;
   do
   {
-    string option_str;
     cout << "Options:" << endl;
     cout << "1) Encrypt" << endl;
     cout << "2) Decrypt" << endl << endl;
     cout << "What do you want to do? ";
 
-    getline(cin, option_str);
-    option_str.erase(remove(option_str.begin(), option_str.end(), ' '), option_str.end());
+    getline(cin, option);
+    option.erase(remove(option.begin(), option.end(), ' '), option.end());
+    option.erase(remove(option.begin(), option.end(), '\t'), option.end());
     cout << endl;
 
-    if(option_str == "1")
-    {
-      option = 1;
-      break;
-    }
-    else if(option_str == "2")
-    {
-      option = 2;
-      break;
-    }
-    else
-    {
-      cout << " ** That is not a valid option. **" << endl << endl;
-    }
+    if      (option == "1") return run_single_key_encrypt();
+    else if (option == "2") return run_single_key_decrypt();
+    else cout << " ** That is not a valid option. **" << endl << endl;
   }while(1);
-
-  if(option == 1)
-  {
-    cout << "************************* ENCRYPTING *************************" << endl << endl;
-    string message;
-    string key;
-
-    cout << "Give me a message to encrypt:" << endl;
-    getline(cin, message);
-    cout << endl;
-
-    cout << "Give me the key to encrypt the message with:" << endl;
-    getline(cin, key);
-    cout << endl;
-
-    if(key.length() < 15)
-    {
-       cout << "NOTE: You should think about making the key longer so that it is harder to crack." << endl << endl;
-    }
-
-    encrypter->set_single_key(key);
-    string encrypted_msg = encrypter->single_key_encrypt(message);
-
-    cout << endl << endl;
-    cout << "\"" << message << "\"" << endl;
-    cout << "encrypted with \"" << key << "\" is:" << endl << endl;
-    cout << "\"" << encrypted_msg << "\"" << endl;
-    delete encrypter;
-    return 0;
-  }
-  else if(option == 2)
-  {
-    cout << "************************* DECRYPTING *************************" << endl << endl;
-    string encrypted_msg;
-    string key;
-
-    cout << "Give me the encrypted message: ";
-    getline(cin, encrypted_msg);
-    cout << endl;
-
-    cout << "And what is the key? ";
-    getline(cin, key);
-
-    encrypter->set_single_key(key);
-    string message = encrypter->single_key_decrypt(encrypted_msg);
-
-    cout << endl << endl;
-    cout << "\"" << encrypted_msg << "\"" << endl;
-    cout <<"decrypted with \"" << key << "\" is:" << endl << endl;
-    cout << "\"" << message << "\"" << endl;
-    delete encrypter;
-    return 0;
-  }
-  else
-  {
-    cout << "Error: Got out of Single-Key Option selection with invalid option." << endl;
-    return 1;
-  }
 }
 
-int run_RSA_option()
+int run_single_key_encrypt()
+{
+  cout << "************************* ENCRYPTING *************************" << endl << endl;
+
+  Encrypter* encrypter = new Encrypter();
+  string message;
+  string key;
+
+  cout << "Give me a message to encrypt:" << endl;
+  getline(cin, message);
+  cout << endl;
+
+  cout << "Give me the key to encrypt the message with:" << endl;
+  getline(cin, key);
+  cout << endl;
+
+  if(key.length() < 15)
+  {
+     cout << "NOTE: You should think about making the key longer so that it is harder to crack." << endl << endl;
+  }
+
+  encrypter->set_single_key(key);
+  string encrypted_msg = encrypter->single_key_encrypt(message);
+
+  cout << endl << endl;
+  cout << "\"" << message << "\"" << endl;
+  cout << "encrypted with \"" << key << "\" is:" << endl << endl;
+  cout << "\"" << encrypted_msg << "\"" << endl;
+
+  delete encrypter;
+  return 0;
+}
+
+int run_single_key_decrypt()
+{
+  cout << "************************* DECRYPTING *************************" << endl << endl;
+
+  Encrypter* encrypter = new Encrypter();
+  string encrypted_msg;
+  string key;
+
+  cout << "Give me the encrypted message: ";
+  getline(cin, encrypted_msg);
+  cout << endl;
+
+  cout << "And what is the key? ";
+  getline(cin, key);
+
+  encrypter->set_single_key(key);
+  string message = encrypter->single_key_decrypt(encrypted_msg);
+
+  cout << endl << endl;
+  cout << "\"" << encrypted_msg << "\"" << endl;
+  cout <<"decrypted with \"" << key << "\" is:" << endl << endl;
+  cout << "\"" << message << "\"" << endl;
+
+  delete encrypter;
+  return 0;
+}
+
+int run_RSA()
 {
     cout << "************************* 2-KEY (RSA) ************************" << endl << endl;
 
