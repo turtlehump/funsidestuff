428084880d2a27dae60f769d770307b52e601379
Philip Vierhus <pvierhus@gmail.com>
2022-01-23 20:16:49 +1100

    Encrypter- File decrypting works :)

--- encryption/main.cpp
+++ encryption/main.cpp
@@ -33,13 +33,16 @@ void run_message_program(bool show);
 
   void run_single_key(bool show);
     void run_single_key_encrypt(bool show);
-    void run_single_key_decrypt(bool show);
+      string get_encrypting_message();
       string get_single_encryption_key();
+
+    void run_single_key_decrypt(bool show);
+      string get_decrypting_message();
       string get_single_decryption_key();
 
   int run_RSA();
 
-    string get_message(bool encrypting);
+    string get_encrypting_message();
 
 void run_file_program(string file_name);
 
@@ -47,7 +50,11 @@ void run_file_program(string file_name);
 
   void file_encrypt(string file_name);
     void file_encryption_warning_option(ofstream & output);
+    bool encrypt_the_file(ifstream &input, ofstream &output, string key);
   void file_decrypt(string file_name);
+    void detect_warning(ifstream &input);
+    string get_file_decryption_key(string filename);
+    bool decrypt_the_file(ifstream &input, ofstream &output, string key);
 //FUNCTIONS ^
 
 int main(int argc, char* argv[])
@@ -168,7 +175,7 @@ void run_single_key_encrypt(bool show)
 {
   cout << "************************* ENCRYPTING *************************" << endl << endl;
 
-  string msg = get_message(true);
+  string msg = get_encrypting_message();
   string key = get_single_encryption_key();
 
   string encrypted_msg = encrypter->single_key_encrypt(msg, key, show);
@@ -185,7 +192,7 @@ void run_single_key_decrypt(bool show)
 {
   cout << "************************* DECRYPTING *************************" << endl << endl;
 
-  string encrypted_msg = get_message(false);
+  string encrypted_msg = get_decrypting_message();
   string key = get_single_decryption_key();
 
   string msg = encrypter->single_key_decrypt(encrypted_msg, key, show);
@@ -198,7 +205,7 @@ void run_single_key_decrypt(bool show)
   return;
 }
 
-string get_message(bool encrypting)
+string get_encrypting_message()
 {
   string msg;
   bool valid_str;
@@ -331,17 +338,17 @@ void run_file_program(string file_name)
 
 void file_encrypt(string file_name)
 {
+  cout << "************************* ENCRYPTING *************************" << endl << endl;
+
   //We know that the file name exists (correct_usage())
   ifstream input(file_name, ios::in);
 
   //We are going to use this file to write the encryption to and then
   //mv this file over the input file
-  //I dont want to use a string in case the file is too large
-  //for a single string
-  string output_file_name = "file.encrypted.tmp";
-  ofstream tmp_output(output_file_name, ios::out);
+  string output_file = "file.encrypted.tmp";
+  ofstream output(output_file, ios::out);
 
-  if(!tmp_output)
+  if(!output)
   {
     cout << "Something went wrong with opening the tmp file needed to do the encrypting." << endl;
     cout << "Nothing has happened with your file." << endl << endl;
@@ -349,39 +356,26 @@ void file_encrypt(string file_name)
     return;
   }
 
-  file_encryption_warning_option(tmp_output);
+  file_encryption_warning_option(output);
 
   string key = get_single_encryption_key();
 
-  char tmp;
-  int key_i = 0;
-  while(input >> tmp)
+  if(encrypt_the_file(input, output, key))
   {
-    if(encrypter->is_valid_char(tmp))
-    {
-      tmp_output << encrypter->encrypt_char(tmp, key[key_i]);
-      key_i++;
-      key_i = key_i % key.length();
-    }
-    else
-    {
-      cout << "There was an invalid character ('" << tmp << "') somewhere in the file." << endl;
-      cout << "Nothing has been done to your file." << endl << endl;
+    input.close();
+    output.close();
 
-      tmp_output.close();
-      remove(output_file_name.c_str());
- 
-      cout << "Exiting" << endl;
-      return;
-    }
+    filesystem::permissions
+      (output_file,
+       filesystem::perms::owner_exec  |
+       filesystem::perms::owner_write ,
+       filesystem::perm_options::remove);
+
+    filesystem::rename(output_file, file_name);
+
+    cout << file_name << " has been overwritten and there is no undoing it without the key" << endl << endl;
   }
-  
-//  tmp_output << endl;
-  tmp_output.close();
-
-  rename(output_file_name.c_str(), file_name.c_str()); //over writes old file
-
-  cout << file_name << " has been overwritten and there is no undoing it without the key" << endl << endl;
+  else filesystem::remove(output_file);
 
   return;
 }
@@ -416,7 +410,141 @@ void file_encryption_warning_option(ofstream & output)
   }while(1);
 }
 
-void file_decrypt(string filename)
+bool encrypt_the_file(ifstream &input, ofstream &output, string key)
 {
+  char tmp;
+  int key_i = 0;
+
+  while(input.get(tmp))
+  {
+    if(encrypter->is_valid_char(tmp))
+    {
+      output << encrypter->encrypt_char(tmp, key[key_i]);
+
+      key_i++;
+      key_i = key_i % key.length();
+    }
+    else
+    {
+      cout << "There was an invalid character ('" << tmp << "') somewhere in the file." << endl;
+      cout << "Nothing has been done to your file." << endl << endl;
+
+      input.close();
+      output.close();
+
+      cout << "Exiting" << endl;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void file_decrypt(string file_name)
+{
+  cout << "************************* DECRYPTING *************************" << endl << endl;
+
+  //we know the file exists (correct_usage())
+  ifstream input(file_name, ios::in);
+
+  string output_file = file_name + ".decrypted";
+  ofstream output(output_file, ios::out);
+
+  if(!output)
+  {
+    cout << "Something has happened and the output file could not be opened" << endl;
+    cout << "Honestly, I have no idea what youre going to do..." << endl;
+    cout << "You probably dont have permissions to create a file here." << endl;
+    input.close();
+    cout << "Nothing has happened to you original file." << endl;
+    return;
+  }
+
+  string key = get_file_decryption_key(file_name);
+
+  detect_warning(input);
+
+  if(decrypt_the_file(input, output, key))
+  {
+    input.close();
+    output.close();
+
+    filesystem::permissions
+      (output_file,
+       filesystem::perms::owner_write ,
+       filesystem::perm_options::add);
+
+    cout << "Your file has been decrypted with key with \"" << key << "\"" << endl;
+    cout << "You still have the encrypted version in case the key was wrong." << endl;
+  }
+  else filesystem::remove(output_file);
+
   return;
+}
+
+//The warning will be the same for every encrypted file
+//The encrypted file should have never been edited (removed writing permissions)
+//
+//If the warning is there, input stream should consume it (return true)
+//If the warning is NOT there, need to reset the input stream (return false)
+void detect_warning(ifstream &input)
+{
+  string first_warning_line = "** This file has been encrypted using github";
+  char* warning = new char[first_warning_line.length()];
+
+  input.get(warning, first_warning_line.length() + 1); //why is +1 needed...?
+
+  if(warning == first_warning_line)
+  {
+    //There are 5 lines of "warning" we need to consume
+    string tmp;
+    getline(input, tmp); //1
+    getline(input, tmp); //2
+    getline(input, tmp); //3
+    getline(input, tmp); //4
+    getline(input, tmp); //5
+  }
+  else input.seekg(0, input.beg);
+
+  return;
+}
+
+string get_file_decryption_key(string file_name)
+{
+  string key;
+  cout << "What is the key for \"" << file_name << "\"?" << endl;
+  do
+  {
+    getline(cin, key);
+  }while(key.length() == 0);
+  cout << endl;
+
+  return key;
+}
+
+bool decrypt_the_file(ifstream &input, ofstream &output, string key)
+{
+  char tmp;
+  int key_i = 0;
+  while(input.get(tmp))
+  {
+    if(encrypter->is_valid_char(tmp))
+    {
+      output << encrypter->decrypt_char(tmp, key[key_i]);
+      key_i++;
+      key_i = key_i % key.length();
+    }
+    else
+    {
+      cout << "There was an invalid character ('" << tmp << "') somewhere in the file." << endl;
+      cout << "Nothing has been done to your file." << endl << endl;
+
+      input.close();
+      output.close();
+
+      cout << "Exiting" << endl;
+      return false;
+    }
+  }
+  return true;
 }
